0eaad0d829c22768606696880a5004b32dd688e1
unknown <testalt369@gmail.com>
2026-08-14 14:55:06 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <p>Welcome to my application.</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
